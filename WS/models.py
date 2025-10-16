a658b6f498194de578ad18d5394c26e8048e5686
yoom <you@example.com>
2025-10-16 21:08:51 +0300

    ggg

--- WS/models.py
+++ WS/models.py
@@ -1,67 +1,76 @@
 from django.db import models
 
 
-class User():
-    # ???
-    pass
+class Bikes(models.Model):
+    bike_types = [
+        (1, "Sport"),
+        (2, "Mountain"),
+        (3, "Town"),
+        (4, "Light"),
+        
+    ]
 
+    # count = [
+    #     (1, "-"),
+    #     (2, "-"),
+    #     (3, "-"),
+    # ]
 
-
-class Product_type(models.Model):
-    Ready_to_sell_Bikes = models.CharField(max_length=15)
-    Wheels = models.CharField(max_length=20)
-    Bike_gear = models.CharField(max_length=20)
-    Accessories = models.CharField(max_length=20)
-    # x = models.CharField()
-    # x = models.CharField()
-
-
-class Bike_Type(models.Model):
-    name = models.CharField(max_length=20)
+    bike_type = models.IntegerField(choices=bike_types, default=4)
+    
+    name = models.CharField(max_length=25)
     description = models.TextField(max_length=1024)
-    Mountain_bikes = models.CharField(50)
-    City_bikes = models.CharField(50)
-    Light_bikes = models.CharField(50)
-    Sport_bikes = models.CharField(50)
-    Price = models.IntegerField(10)
-    x = models.CharField()
-    
+    price = models.IntegerField(max_length=6, default=None)
+    is_there_any = models.BooleanField(default=False)
+    bike_amount = models.IntegerField()
 
 
-# class Bike_Price(self):
-#     x = models.CharField()
-#     x = models.CharField()
-#     x = models.CharField()
-#     x = models.CharField()
-#     x = models.CharField()
-#     x = models.CharField()
 
 
-    
-class Wheel_type(models.Model):
-    Mountain = models.CharField(50)
-    City = models.CharField(50)
-    Light = models.CharField(50)
-    Sport = models.CharField(50)
+
+
+
+
+
+
+
+# class Product_type(models.Model):
+#     Ready_to_sell_Bikes = models.CharField(max_length=15)
+#     Wheels = models.CharField(max_length=20)
+#     Bike_gear = models.CharField(max_length=20)
+#     Accessories = models.CharField(max_length=20)
     # x = models.CharField()
     # x = models.CharField()
 
 
+# class Bike_Type(models.Model):
+#     name = models.CharField(max_length=20)
+#     description = models.TextField(max_length=1024)
+#     Mountain_bikes = models.CharField(50)
+#     City_bikes = models.CharField(50)
+#     Light_bikes = models.CharField(50)
+#     Sport_bikes = models.CharField(50)
+#     Price = models.IntegerField(10)
+#     x = models.CharField()
     
-# class Wheel_price(self):
-#     x = models.CharField()
-#     x = models.CharField()
-#     x = models.CharField()
-#     x = models.CharField()
-#     x = models.CharField()
+
+    
+# class Wheel_type(models.Model):
+#     Mountain = models.CharField(50)
+#     City = models.CharField(50)
+#     Light = models.CharField(50)
+#     Sport = models.CharField(50)
+    # x = models.CharField()
+    # x = models.CharField()
 
 
+
     
-class Wheel_size(models.Model):
-    twenty_d = models.IntegerField(5)
-    twenty_four_d = models.IntegerField(5)
-    twenty_six_d = models.IntegerField(5)
-    twenty_eight_d = models.IntegerField(5)
-    twenty_nine_d = models.IntegerField(5)
+# class Wheel_size(models.Model):
+#     twenty_d = models.IntegerField(5)
+#     twenty_four_d = models.IntegerField(5)
+#     twenty_six_d = models.IntegerField(5)
+#     twenty_eight_d = models.IntegerField(5)
+#     twenty_nine_d = models.IntegerField(5)
 
     
--- WS/urls.py
+++ WS/urls.py
@@ -3,13 +3,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from WS.views import HomeView, ShopView, SearchView, OrderView, BikeView 
+from WS.views import HomeView, ShopView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='greetings_page'),
     path('store/', ShopView.as_view(), name='main_store_page'),
-    path('search/', SearchView.as_view(), name='search_page'),
-    path('order/', OrderView.as_view(), name='order_page'),
-    path('bike_description/', BikeView.as_view(), name='bike_description_page')
+    # path('search/', SearchView.as_view(), name='search_page'),
+    # path('order/', OrderView.as_view(), name='order_page'),
+    # path('bike_description/', BikeView.as_view(), name='bike_description_page')
     
 ]
